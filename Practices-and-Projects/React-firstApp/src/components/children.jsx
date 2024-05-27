cb21ff12c12145c28ee18ad4d067f55b9ebdb767
Yokii <70013018+YokiiEditz@users.noreply.github.com>
2024-05-27 16:14:45 +0530

    Create children.jsx

---
+++ Practices-and-Projects/React-firstApp/src/components/children.jsx
@@ -0,0 +1,24 @@
+
+// PARENT Component:
+
+// <Demo value1={num1} value2={num2}>
+// <div>This is demo statement</div>
+// </Demo>
+
+
+// CHILD Component:
+
+// const Demo = ({ value1, value2, children }) => {
+//   return (
+//     <div>
+//       <div>
+//         <p>{value1}</p>
+//         <p>{value2}</p>
+//         result: {value1 + value2}
+//         {children}
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Demo;
